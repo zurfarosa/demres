933a762bb8820131238165a61fcc01f16bf1a633
Richard Hoile <richard.hoile@gmail.com>
2017-01-13 01:47:45 +0000

    monthly insomnia event count created

--- my_functions.py
+++ my_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import csv
 
 
@@ -19,7 +20,7 @@ def create_pegprod():
     """
     Creates a cleaned up version of Pegasus Products dictionary in csv form
     """
-    raw_pegprod = pd.read_csv('data/dicts/raw_pegasus_product.txt',delimiter='\t',encoding='latin-1',header=None)
+    raw_pegprod = pd.read_csv('data/dicts/raw_pegasus_product.txt',delimiter='\t',encoding='latin-1', skiprows=[0,1],header=None)
     raw_pegprod.columns=['product code','XXX code','therapy events','product name','drug substance name','substance strength','formulation','route','BNF code','BNF header','database build','unknown column']
     raw_pegprod.to_csv('data/dicts/proc_pegasus_prod.csv',index=False)
 
@@ -52,15 +53,26 @@ def create_insomnia_clinentries():
     relev_clinical = df_joined[df_joined['medcode'].isin(medcodes)]
     relev_clinical.to_csv('data/pt_data/proc_insomnia_clinicalentries.csv',index=False)
 
+def clean_matching():
+    """
+    Removes the 196 rows from the 'matching' dataframe where there is no control matching the case,
+    then creates a files called 'unmatched_cases' for the lone cases, and a file called
+    'matching_unmatched_removed.csv' for the cleaned up matched pairs
+    """
+    matching = pd.read_csv('data/pt_data/Matching_File.txt',delimiter='\t')
+    matching_unmatched_removed = matching[matching['control_pracid']!='.']
+    matching_unmatched_removed.to_csv('data/pt_data/matching_unmatched_removed.csv',index=False)
+    unmatched_cases = matching[matching['control_patid']=='.'][['case_patid','case_pracid','case_gender','case_birthyear','case_index']]
+    unmatched_cases.columns=['patID','pracID','gender','birthyear','case_index']
+    unmatched_cases.to_csv('data/pt_data/ummatched_cases.csv',index=False)
+
 def create_pt_features():
     '''
     Creates csv file containing all patients (cases and controls on separate
     rows) with a column for all variables for logistic regression
     '''
 
-    #delete rows where there is no control for the case
-    # matching = matching[matching.]
-
+    matching = pd.read_csv('data/pt_data/matching_unmatched_removed.csv',delimiter=',')
 
     #Create the cases
     feature1 = matching.copy(deep=True)
@@ -93,10 +105,24 @@ def create_pt_features():
     features = pd.concat([feature1,feature2])
     features=features[['patID','pracID','gender','birthyear','case_index','matchID','data_start','data_end','isCase']]
 
+    features['case_index']=pd.to_datetime(features['case_index'], format='%d/%m/%Y', errors='coerce')
+    features['data_start']=pd.to_datetime(features['data_start'], format='%d/%m/%Y', errors='coerce')
+    features['data_end']=pd.to_datetime(features['data_end'], format='%d/%m/%Y', errors='coerce')
+
+    # the np.timedelta64 divider allows us to convert the timedelta.days format into an integer
+    features['prediagnosis_data_length_in_days']=((features['case_index']-features['data_start']) / np.timedelta64(1, 'D')).astype(int)
+
     features.to_csv('data/pt_data/pt_features.csv',index=False)
 
+def create_monthly_insom_event_count_for_each_patient():
+    insom_ents = pd.read_csv('data/pt_data/proc_insomnia_clinicalentries.csv',delimiter=',')
+    insom_ents=insom_ents[pd.notnull(insom_ents['eventdate'])] #drops a small number of rows (only about 64) with NaN eventdates
+    insom_ents['eventdate']=pd.to_datetime(insom_ents['eventdate'])
+    insom_ents_resampled=insom_ents[['patid','eventdate']].set_index('eventdate').groupby('patid').resample('M').count()
+    #convert back to dataframe
+    insom_ents_resampled_dataframe = insom_ents_resampled.add_suffix('_count').reset_index()
+    insom_ents_resampled_dataframe.columns=['patid','eventdate','insom_count']
+    #delete zero counts
+    insom_ents_resampled_dataframe=insom_ents_resampled_dataframe[insom_ents_resampled_dataframe['insom_count']>0]
 
-
-# def get_insomnia_clinentries():
-#     df = pd.read_csv('data/pt_data/proc_insomnia_clinicalentries.csv',delimiter='\t')
-#     print(df.head(10))
+    insom_ents_resampled_dataframe.to_csv('data/pt_data/monthly_insom_event_count.csv',index=False)
