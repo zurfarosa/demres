709bf7db293021c8f19ccd29c41898318b98158a
Richard Hoile <richard.hoile@gmail.com>
2017-01-18 16:17:33 +0000

    created a match function

--- constants.py
+++ constants.py
@@ -1,10 +1,11 @@
-class Constants:
-    exposure_start = 5 #minimum years prior to index date that insomnia episodes are counted as exposure
-    exposure_end = 10 #maximum years prior to index date that insomnia episodes are counted as exposure
-
 class Entry_Type:
     consultation = 0
     referral = 1
     clinical = 2
     test = 3
     look_up = {consultation:'consultation',referral:'referral',clinical:'clinical',test:'test'}
+
+class Study_Design:
+    years_between_exposure_measurement_and_index_date = 5
+    years_of_exposure_measurement = 5
+    total_years_required_pre_index_date = years_of_exposure_measurement + years_of_exposure_measurement
--- my_functions.py
+++ my_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import csv
-from constants import Entry_Type
+from constants import Entry_Type, Study_Design
 
 
 """HELPER FUNCTIONS"""
@@ -41,9 +41,9 @@ def get_insomnia_medcodes():
     medcodes = pd.read_csv('data/codelists/insomnia_medcodes.csv',delimiter=',')
     return [int(medcode) for medcode in list(medcodes)]
 
-def create_medcoded_clinical_entries():
+def create_medcoded_entries():
     """
-    Creates create_medcoded_clinical_entries.csv
+    Creates create_medcoded_entries.csv
     This is a file containing a dataframe containing simplified data
     (just patient ID, eventdate, sysdate, and medcode) from the
     Extract_Clinical_001 and 002 files, Extract_Test_001 and 002 file and Extract_Referral_001 file
@@ -73,26 +73,36 @@ def create_medcoded_clinical_entries():
     referral['sysdate'] = pd.to_datetime(referral['sysdate'], format='%d/%m/%Y', errors='coerce')
     referral['type']='referral'
 
-    medcoded_clinical_entries = pd.concat([clinical,test,referral])
-    medcoded_clinical_entries.to_csv('data/pt_data/medcoded_clinical_entries.csv',index=False)
+    medcoded_entries = pd.concat([clinical,test,referral])
+    medcoded_entries.to_csv('data/pt_data/medcoded_entries.csv',index=False)
 
 def create_consultations():
     """
     Creates a csv file containing all the data from Extract_Consultation_001.txt and Extract_Consultation_002.txt
     """
-    cons1 = pd.read_csv('data/pt_data/Extract_Consultation_001',delimiter='\t')
+    cons1 = pd.read_csv('data/pt_data/Extract_Consultation_001.txt',delimiter='\t')
     cons2 = pd.read_csv('data/pt_data/Extract_Consultation_002.txt',delimiter='\t')
-    consultations = pd.concat([cons1,cons2])[['patid','sysdate','eventdate']] #note that consultations don't have a medcode - create a blank medcode column just makes it easier to concatenate with all the other clinical entries
-    consultations['medcode']=np.nan
+    consultations = pd.concat([cons1,cons2])[['patid','sysdate','eventdate']]
     consultations['eventdate'] = pd.to_datetime(consultations['eventdate'], format='%d/%m/%Y', errors='coerce')
     consultations['sysdate'] = pd.to_datetime(consultations['sysdate'], format='%d/%m/%Y', errors='coerce')
     consultations['type']= Entry_Type.consultation
+    consultations.to_csv('data/pt_data/consultations.csv',index=False)
+
+def create_all_entries():
+    """
+    Creates a csv file (all_entries.csv) containing all all_entries (consultations, plus the medcoded entries - clinicals, tests, referrals)
+    The csv file is only used as  a way of estimating the dates of data extraction (hopefully CPRD will tell me how to do this properly soon)
+    """
+    consultations = pd.read_csv('data/pt_data/consultations.csv',delimiter=',')
+    medcoded_entries = pd.read_csv('data/pt_data/medcoded_entries.csv',delimiter=',')
+    all_entries = pd.concat([consultations,medcoded_entries])
+    all_entries.to_csv('data/pt_data/all_entries.csv',index=False)
 
 def create_insomnia_entries():
     """
-    Creates csv file containing all entries (tests, referrals, 'clinicals') relating to our insomnia medcodes
+    Creates csv file containing all medcoded entries (tests, referrals, clinicals) relating to our insomnia medcodes
     """
-    df = pd.read_csv('data/pt_data/all_clinical_tests_referrals_just_patid_and_dates.csv',delimiter=',')
+    df = pd.read_csv('data/pt_data/medcoded_entries.csv',delimiter=',')
     medcodes = get_insomnia_medcodes()
     relev_clinical = df[df['medcode'].isin(medcodes)]
     relev_clinical.to_csv('data/pt_data/all_insomnia_entries.csv',index=False)
@@ -106,10 +116,9 @@ def clean_matching():
     matching = pd.read_csv('data/pt_data/Matching_File.txt',delimiter='\t')
     matching_unmatched_removed = matching[matching['control_pracid']!='.']
     matching_unmatched_removed.to_csv('data/pt_data/matching_unmatched_removed.csv',index=False)
-    unmatched_cases = matching[matching['control_patid']=='.'][['case_patid','case_pracid','case_gender','case_birthyear','case_index']]
-    unmatched_cases.columns=['patID','pracID','gender','birthyear','case_index']
+    unmatched_cases = matching[matching['control_patid']=='.'][['case_patid','case_pracid','case_gender','case_birthyear','index_date']]
+    unmatched_cases.columns=['patid','pracID','gender','birthyear','index_date']
     unmatched_cases.to_csv('data/pt_data/ummatched_cases.csv',index=False)
-
 
 def create_pt_features():
     '''
@@ -118,83 +127,90 @@ def create_pt_features():
     '''
 
     matching = pd.read_csv('data/pt_data/matching_unmatched_removed.csv',delimiter=',')
+    del(matching['match'])
+    del(matching['control_end'])
+    del(matching['control_start'])
 
     #Create the cases
     feature1 = matching.copy(deep=True)
-    del(feature1['match'])
-    del(feature1['control_birthyear'])
-    del(feature1['control_pracid'])
-    del(feature1['control_gender'])
+    # reorder the columns
+    feature1=feature1[['case_patid','case_pracid','case_gender','case_birthyear','case_index','control_patid']]
     feature1['isCase']=True
-    feature1.columns=['patID','pracID','gender','birthyear','case_index','original_matchID','data_start','data_end','isCase']
+    # rename the columns
+    feature1.columns=['patid','pracid','gender','birthyear','index_date','original_matchid','isCase']
 
     #Create the controls
     feature2 = matching.copy(deep=True)
-    del(feature2['match'])
-    del(feature2['case_pracid'])
-    del(feature2['case_birthyear'])
-    del(feature2['case_gender'])
+    # reorder the columns
+    feature2=feature2[['control_patid','control_pracid','control_gender','control_birthyear','case_index','control_patid']]
     feature2['isCase']=False
-    cols = ['matchID',
-     'case_index',
-     'patID',
-     'pracID',
-     'gender',
-     'birthyear',
-     'data_start',
-     'data_end',
-     'isCase']
-    feature2.columns=cols
+    # rename the columns
+    feature2.columns=['patid','pracid','gender','birthyear','index_date','original_matchid','isCase']
 
     #Merge cases and controls
     features = pd.concat([feature1,feature2])
-    features=features[['patID','pracID','gender','birthyear','case_index','original_matchID','data_start','data_end','isCase']]
+    features=features[['patid','pracid','gender','birthyear','index_date','original_matchid','isCase']]
 
-    features['case_index']=pd.to_datetime(features['case_index'], format='%d/%m/%Y', errors='coerce')
-    features['data_start']=pd.to_datetime(features['data_start'], format='%d/%m/%Y', errors='coerce')
-    features['data_end']=pd.to_datetime(features['data_end'], format='%d/%m/%Y', errors='coerce')
+    features['index_date']=pd.to_datetime(features['index_date'], format='%d/%m/%Y', errors='coerce')
+    # features['data_start']=pd.to_datetime(features['data_start'], format='%d/%m/%Y', errors='coerce')
+    # features['data_end']=pd.to_datetime(features['data_end'], format='%d/%m/%Y', errors='coerce')
 
     # the np.timedelta64 divider allows us to convert the timedelta.days format into an integer
-    features['prediagnosis_data_length_in_days']=((features['case_index']-features['data_start']) / np.timedelta64(1, 'D')).astype(int)
+    # features['prediagnosis_data_length_in_days']=((features['index_date']-features['data_start']) / np.timedelta64(1, 'D')).astype(int)
 
     features.to_csv('data/pt_data/pt_features.csv',index=False)
 
 def add_sys_start_and_end_dates_plus_length_of_available_data_to_patient_features(all_entries):
     '''
-    Needs the dateframe created by create_medcoded_clinical_entries() to be passed to it.
+    Needs the dateframe created by create_medcoded_entries() to be passed to it.
     This will need to be rewritten when I know the PROPER way to get the start and end dates for each patient's period of data extraction.
-    As it stands, this function looks at all the medcoded_clinical_entries, and looks for the first and last 'sysdated' entry.
+    As it stands, this function looks at all the medcoded_entries, and looks for the first and last 'sysdated' entry.
     '''
     pt_features = pd.read_csv('data/pt_data/pt_features.csv',delimiter=',')
 
-    pt_features['sysdate_start']=pt_features['patID'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').head(1)['sysdate'].values[0])
+    pt_features['sysdate_start']=pt_features['patid'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').head(1)['sysdate'].values[0])
     pt_features['sysdate_start'] = pd.to_datetime(pt_features['sysdate_start'])
 
-    pt_features['sysdate_end']=pt_features['patID'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').tail(1)['sysdate'].values[0])
+    pt_features['sysdate_end']=pt_features['patid'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').tail(1)['sysdate'].values[0])
     pt_features['sysdate_end'] = pd.to_datetime(pt_features['sysdate_end'])
 
-    pt_features['case_index'] = pd.to_datetime(pt_features['case_index'])
+    pt_features['index_date'] = pd.to_datetime(pt_features['index_date'])
 
-    pt_features['length_of_data_pre_index_date']=((pt_features['case_index']-pt_features['sysdate_start'])/ np.timedelta64(1, 'D')).astype(int)
-    pt_features['length_of_data_post_index_date']=((pt_features['sysdate_end']-pt_features['case_index'])/ np.timedelta64(1, 'D')).astype(int)
+    pt_features['length_of_data_pre_index_date']=((pt_features['index_date']-pt_features['sysdate_start'])/ np.timedelta64(1, 'D')).astype(int)
+    pt_features['length_of_data_post_index_date']=((pt_features['sysdate_end']-pt_features['index_date'])/ np.timedelta64(1, 'D')).astype(int)
 
+    pt_features.to_csv('data/pt_data/pt_features.csv',index=False)
+
+def delete_patients_if_not_enough_data():
+    pt_features = pd.read_csv('data/pt_data/pt_features.csv',delimiter=',')
+
+    #delete patients if not enough data prior to index dates
+    removed_pt_features = pt_features[isCase == True & pt_features[length_of_data_pre_index_date]<(Study_Design.total_years_required_pre_index_date*365)]
+    removed_pt_features['reason']='Not enough available data prior to index date'
+    removed_pt_features.to_csv('data/pt_data/removed_pts.csv',index=False)
+    pt_features=pt_features-removed_pt_features
     pt_features.to_csv('data/pt_data/pt_features.csv',index=False)
 
 def match_cases_and_controls():
     pt_features = pd.read_csv('data/pt_data/pt_features.csv',delimiter=',')
+    pt_features['new_match_id']=np.nan
     for index,row in pt_features.iterrows():
         if pd.isnull(row['new_match_id']):
-            patid = row['patID']
+            patid = row['patid']
+            print(patid)
             birthyear = row['birthyear']
             gender = row['gender']
             isCase = row['isCase']
+            pracid = row['pracid']
             matches_birthyear = pt_features['birthyear']==birthyear
             matches_gender = pt_features['gender']==gender
-            does_not_match_isCase = pt_features['gender'] != isCase
+            differs_in_caseness = pt_features['isCase'] != isCase
+            matches_practice = pt_features['pracid']==pracid
             is_not_already_matched = pd.isnull(pt_features['new_match_id'])
-            matching_pt = df[matches_birthyear & matches_gender & does_not_match_isCase & is_not_already_matched].head(1)
+            matching_pt = pt_features[matches_birthyear & matches_gender & matches_practice & differs_in_caseness & is_not_already_matched].head(1)
             matching_pt['new_match_id']=patid
-            row['new_match_id']=matching_pt['patid']
+            pt_features.loc[index,'new_match_id']=matching_pt['patid'].values[0]
+    pt_features.to_csv('data/pt_data/pt_features.csv',index=False)
 
 
 def create_monthly_insom_event_count_for_each_patient():
