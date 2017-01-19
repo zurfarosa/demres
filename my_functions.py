bbd35595a4a093314a8d112f063862cebaa9db0f
Richard Hoile <richard.hoile@gmail.com>
2017-01-19 09:59:53 +0000

    finished the insomnia count function

--- my_functions.py
+++ my_functions.py
@@ -1,13 +1,8 @@
 import pandas as pd
 import numpy as np
 import csv
+from datetime import date, timedelta
 from constants import Entry_Type, Study_Design
-
-
-"""HELPER FUNCTIONS"""
-def lookup_one_to_one(df,lookup_col,new_col, lookupval):
-    return df.loc[df[lookup_col]==lookupval][new_col].iloc[0]
-
 
 def create_pegmed():
     """
@@ -32,14 +27,10 @@ def create_insomnia_medcodes():
     """
     pegmed = pd.read_csv('data/dicts/proc_pegasus_medical.csv')
     readcodes = pd.read_csv('data/codelists/insomnia_readcodes.csv',delimiter=',')
-    medcodes=[str(lookup_one_to_one(pegmed,'readcode','medcode',readcode)) for readcode in readcodes]
+    medcodes=[str(pegmed.loc[pegmed['readcode']==readcode]['medcode'].iloc[0]) for readcode in readcodes]
     with open('data/codelists/insomnia_medcodes.csv','w', newline='') as f:
         writer = csv.writer(f,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(medcodes)
-
-def get_insomnia_medcodes():
-    medcodes = pd.read_csv('data/codelists/insomnia_medcodes.csv',delimiter=',')
-    return [int(medcode) for medcode in list(medcodes)]
 
 def create_medcoded_entries():
     """
@@ -97,15 +88,6 @@ def create_all_entries():
     medcoded_entries = pd.read_csv('data/pt_data/medcoded_entries.csv',delimiter=',')
     all_entries = pd.concat([consultations,medcoded_entries])
     all_entries.to_csv('data/pt_data/all_entries.csv',index=False)
-
-def create_insomnia_entries():
-    """
-    Creates csv file containing all medcoded entries (tests, referrals, clinicals) relating to our insomnia medcodes
-    """
-    df = pd.read_csv('data/pt_data/medcoded_entries.csv',delimiter=',')
-    medcodes = get_insomnia_medcodes()
-    relev_clinical = df[df['medcode'].isin(medcodes)]
-    relev_clinical.to_csv('data/pt_data/all_insomnia_entries.csv',index=False)
 
 def clean_matching():
     """
@@ -168,10 +150,10 @@ def add_sys_start_and_end_dates_plus_length_of_available_data_to_patient_feature
     '''
     pt_features = pd.read_csv('data/pt_data/pt_features.csv',delimiter=',')
 
-    pt_features['sysdate_start']=pt_features['patid'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').head(1)['sysdate'].values[0])
+    pt_features['sysdate_start'] = pt_features['patid'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').head(1)['sysdate'].values[0])
     pt_features['sysdate_start'] = pd.to_datetime(pt_features['sysdate_start'])
 
-    pt_features['sysdate_end']=pt_features['patid'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').tail(1)['sysdate'].values[0])
+    pt_features['sysdate_end'] = pt_features['patid'].map(lambda x: all_entries[all_entries['patid']==x].sort_values(by='sysdate').tail(1)['sysdate'].values[0])
     pt_features['sysdate_end'] = pd.to_datetime(pt_features['sysdate_end'])
 
     pt_features['index_date'] = pd.to_datetime(pt_features['index_date'])
@@ -212,16 +194,50 @@ def match_cases_and_controls():
             pt_features.loc[index,'new_match_id']=matching_pt['patid'].values[0]
     pt_features.to_csv('data/pt_data/pt_features.csv',index=False)
 
+def get_insomnia_medcodes():
+    medcodes = pd.read_csv('data/codelists/insomnia_medcodes.csv',delimiter=',')
+    return [int(medcode) for medcode in list(medcodes)]
 
-def create_monthly_insom_event_count_for_each_patient():
-    insom_ents = pd.read_csv('data/pt_data/proc_insomnia_clinicalentries.csv',delimiter=',')
-    insom_ents=insom_ents[pd.notnull(insom_ents['eventdate'])] #drops a small number of rows (only about 64) with NaN eventdates
-    insom_ents['eventdate']=pd.to_datetime(insom_ents['eventdate'])
-    insom_ents_resampled=insom_ents[['patid','eventdate']].set_index('eventdate').groupby('patid').resample('M').count()
-    #convert back to dataframe
-    insom_ents_resampled_dataframe = insom_ents_resampled.add_suffix('_count').reset_index()
-    insom_ents_resampled_dataframe.columns=['patid','eventdate','insom_count']
+def create_insomnia_entries():
+    """
+    Creates csv file containing all medcoded entries (tests, referrals, clinicals) relating to our insomnia medcodes
+    """
+    df = pd.read_csv('data/pt_data/medcoded_entries.csv',delimiter=',')
+    medcodes = get_insomnia_medcodes()
+    relev_clinical = df[df['medcode'].isin(medcodes)]
+    relev_clinical.to_csv('data/pt_data/all_insomnia_entries.csv',index=False)
+
+def add_insomnia_event_count_to_pt_features():
+    """
+    Calculates count of insomnia-months for each patient, and adds it to pt_features dataframe
+    """
+    # Load list of all insomnia entries, then group it to calculate each patient's insomnia count, broken down by month
+    insom_events = pd.read_csv('data/pt_data/all_insomnia_entries.csv',delimiter=',')
+    insom_events=insom_events[pd.notnull(insom_events['eventdate'])] #drops a small number of rows (only about 64) with NaN eventdates
+    insom_events.loc[:,'eventdate']=pd.to_datetime(insom_events['eventdate'])
+    insom_events=insom_events[['patid','eventdate']].set_index('eventdate').groupby('patid').resample('M').count()
+    #convert group_by object back to dataframe
+    insom_events = insom_events.add_suffix('_count').reset_index()
+    insom_events.columns=['patid','eventdate','insom_count']
     #delete zero counts
-    insom_ents_resampled_dataframe=insom_ents_resampled_dataframe[insom_ents_resampled_dataframe['insom_count']>0]
+    insom_events=insom_events[insom_events['insom_count']>0]
 
-    insom_ents_resampled_dataframe.to_csv('data/pt_data/monthly_insom_event_count.csv',index=False)
+    # Restrict insomnia event counts to those that occur during exposure period
+    pt_features = pd.read_csv('data/pt_data/pt_features.csv',delimiter=',')
+    insom_events['pat_index_date'] = insom_events['patid'].map(lambda x: pt_features[pt_features['patid']==x]['index_date'].values[0])
+    insom_events['pat_index_date'] = pd.to_datetime(insom_events['pat_index_date'])
+    too_close_to_dx_period = timedelta(days=365)*Study_Design.years_between_exposure_measurement_and_index_date
+    too_distant_from_dx_period = too_close_to_dx_period + timedelta(days=365)*Study_Design.years_of_exposure_measurement
+    not_too_close_to_dx = insom_events['eventdate']<(insom_events['pat_index_date']-too_close_to_dx_period)
+    not_before_exposure_period = insom_events['eventdate']>(insom_events['pat_index_date']-too_distant_from_dx_period)
+    relev_insom_events = insom_events[not_too_close_to_dx & not_before_exposure_period]
+    # Convert groupby object into a dataframe, and rename new column
+    relev_insom_events_by_pt = relev_insom_events.groupby(by='patid')['eventdate'].count().reset_index()
+    relev_insom_events_by_pt.columns=['patid','insomnia_event_count']
+
+    # merge pt_features with new relev_insomnia_event dataframe
+    merged_table = pd.merge(pt_features,relev_insom_events_by_pt, on='patid',how='outer')
+    merged_table['insomnia_event_count'].fillna(0,inplace=True)
+    merged_table['insomnia_event_count'] = merged_table['insomnia_event_count'].astype(int)
+
+    merged_table.to_csv('data/pt_data/pt_features.csv',index=False)
