45133dd07fc33470fda6a3ad847231c05d6692c9
Richard Hoile <richard.hoile@gmail.com>
2017-03-16 23:52:53 +0000

    minor changes

--- common/logger.py
+++ common/logger.py
@@ -1,6 +1,6 @@
 import logging
 logger = logging.getLogger()
-fhandler = logging.FileHandler(filename='mylog.log', mode='a')
+fhandler = logging.FileHandler(filename='output/mylog.log', mode='a')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fhandler.setFormatter(formatter)
 logger.addHandler(fhandler)
